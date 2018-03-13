3c6e04337b63f4e7f0a075ae9f4d978f932bf6b7
cjohannsen <christoph.johannsen@plentymarkets.com>
2018-03-13 18:08:11 +0100

    CHANGE utils.js - add dynamicArraySort

--- resources/js/src/app/helper/utils.js
+++ resources/js/src/app/helper/utils.js
@@ -13,3 +13,20 @@ export function isNullOrUndefined(object)
 {
     return isNull(object) || isUndefined(object);
 }
+
+export function dynamicArraySort(array, sortingField)
+{
+    return array.sort((valueA, valueB) =>
+    {
+        if (valueA[sortingField] > valueB[sortingField])
+        {
+            return 1;
+        }
+        if (valueA[sortingField] < valueB[sortingField])
+        {
+            return -1;
+        }
+
+        return 0;
+    });
+}
